825eb94ddacede114661fa0bec5bc171df0b3b98
Jorgedavyd <jorged.encyso@gmail.com>
2024-09-24 11:48:06 -0400

    setting up cpp workflow

---
+++ .local/scripts/project_manager/workflows/cpp/include/h.h
@@ -0,0 +1,4 @@
+#ifndef H_H
+#define H_H
+int main(void);
+#endif //H_H
---
+++ .local/scripts/project_manager/workflows/cpp/lib/cpp.cpp
@@ -0,0 +1,6 @@
+#include <h.h>
+#include <iostream>
+int main (void) {
+    std::cout << "sample" << "\n";
+    return 0;
+};
